1de85447d89e3c7b59e1185b66be18ad768d6e11
AlexandruPites <94063546+AlexandruPites@users.noreply.github.com>
2022-05-24 14:01:10 +0300

    Added main and header files

---
+++ header.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+typedef struct node{
+    char table[3][3];
+    struct node **children;
+    int depth;
+    char score;
+}Node, *PNode; 
